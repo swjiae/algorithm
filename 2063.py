bbd95a6e67d2bcdde2a2f15d0fd38373eb8afcec
swjiae <jiae1625@gmail.com>
2022-07-18 23:42:57 +0900

    new start

--- 2063.py
+++ 2063.py
@@ -5,11 +5,13 @@
 # "%" : 나눗셈의 나머지 - 나눗셈 결과의 '나머지'를 가져옴
 # divmod(): 나눗셈의 몫과 나머지(튜플 형식) - 나눗셈 결과의 '몫'과 '나머지'를 한번에 가져옴
 
-N = int(input())
-numbers = list(map(int, input().split()))
+# N = int(input())
+# numbers = list(map(int, input().split()))
 
-numbers.sort()
+# numbers.sort()
 
-m = (N-1)//2
+# m = (N-1)//2
 
-print(numbers[m])
+# print(numbers[m])
+
+divmod(5, 10)
